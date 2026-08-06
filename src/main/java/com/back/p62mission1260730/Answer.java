46b13235e090c005ba3d2b289d371079527e3642
songfox00 <songfox00@gmail.com>
2026-08-06 17:13:28 +0900

    2-04 엔티티로 테이블 매핑하기

---
+++ src/main/java/com/back/p62mission1260730/Answer.java
@@ -0,0 +1,25 @@
+package com.back.p62mission1260730;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
