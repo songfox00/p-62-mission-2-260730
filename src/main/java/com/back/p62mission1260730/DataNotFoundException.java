7ff02fe320625299f338d48fbb544add1cb29d54
songfox00 <songfox00@gmail.com>
2026-08-08 15:59:03 +0900

    2-10 상세 페이지 만들기

---
+++ src/main/java/com/back/p62mission1260730/DataNotFoundException.java
@@ -0,0 +1,12 @@
+package com.back.p62mission1260730;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason = "entity not found")
+public class DataNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+    public DataNotFoundException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/back/p62mission1260730/question/QuestionService.java
+++ src/main/java/com/back/p62mission1260730/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.back.p62mission1260730.question;
 
+import com.back.p62mission1260730.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +14,15 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(int id){
+        Optional<Question> oq = this.questionRepository.findById(id);
+
+        if(oq.isPresent()){
+            return oq.get();
+        }else{
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
